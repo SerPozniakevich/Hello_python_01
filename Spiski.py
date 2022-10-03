028c5878f5fc506e82e0990ebe6c28cb163f0b53
Sergey Pozniakevich <ven07@yandex.ru>
2022-10-03 19:35:12 +0700

    add discripion copy.deepcopy

---
+++ Spiski.py
@@ -0,0 +1,36 @@
+import copy
+
+list1 = [1,2,3,4,5]
+# list2 = list1              # при изенении одного списка, меняется оба списка
+list2 = copy.deepcopy(list1) # глубокое копирование, при изменении одного списка, второй не меняется
+
+for e in list1:
+    print(e)
+
+print()
+
+for e in list2:
+    print(e)
+
+list1[0] = 123 
+list2[1] = 333 
+
+for e in list1:
+    print(e)
+
+print()
+
+for e in list2:
+    print(e)
+
+list1 = [1,2,3,4,5]
+
+# print(list1)
+# print(list1.pop()) # функция .pop без указания элемента удаляет последний элемент
+# print(list1)
+# print(list1.pop(2)) # функция .pop с указанием элемента удаляет указанный элемент
+# print(list1)
+# print(list1.insert(2, 11)) # добавление элемента на позицию 2 (между 2 и 4)
+# print(list1)
+# print(list1.append(11)) # добавление элемента последним
+# print(list1)
